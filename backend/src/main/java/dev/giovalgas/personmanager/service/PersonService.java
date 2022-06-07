492b90ab9091fd1543e273d1297fff3378104569
giovalgas <giovalgascom@gmail.com>
2022-06-07 16:19:30 -0300

    Connect to postgres

---
+++ backend/src/main/java/dev/giovalgas/personmanager/service/PersonService.java
@@ -0,0 +1,30 @@
+package dev.giovalgas.personmanager.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PersonService {
+
+  public void logicallyDeletePerson() {
+
+  }
+
+  public void createPerson() {
+
+  }
+
+  public void editPerson() {
+
+  }
+
+  public void getAllPersonsByFilter(String nameFilter, String emailFilter, boolean enabledFilter) {
+
+  }
+
+  public void getPersonById() {
+
+  }
+
+}
