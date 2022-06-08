c8a8e5e10513d5fba02f24b51f901543b7ff93bf
giovalgas <giovalgascom@gmail.com>
2022-06-08 16:03:49 -0300

    Add basic layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+import './index.css';
+
+import {
+  BarChartOutlined,
+  UploadOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import {Breadcrumb, Layout, Menu, MenuProps} from 'antd';
+const { Header, Content, Footer } = Layout;
+
+
+const items: MenuProps["items"] = [
+    {icon: UserOutlined, key: "Pessoas"}, {icon: UploadOutlined, key: "Adicionar pessoa"}, {icon: BarChartOutlined, key: "Statisticas"}
+].map((i, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(i.icon),
+  label: `${i.key}`,
+}));
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              items={items}
+          />
+        </Header>
+        <Content
+            style={{
+              padding: '0 50px',
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Breadcrumb
+              style={{
+                margin: '16px 0',
+              }}
+          >
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>Pessoas</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">Content</div>
+        </Content>
+        <Footer
+            style={{
+              textAlign: 'center',
+            }}
+        >
+          Radar Saude ©2022 Created by Giovani Valgas
+        </Footer>
+      </Layout>
   );
 }
 
