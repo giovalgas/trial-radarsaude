f94750090fb6d5c54ad668a65aa81c08a13e0de6
giovalgas <giovalgascom@gmail.com>
2022-06-08 01:05:15 -0300

    Remove @NonNull from PersonEntity

--- backend/src/main/java/dev/giovalgas/personmanager/entity/person/PersonEntity.java
+++ backend/src/main/java/dev/giovalgas/personmanager/entity/person/PersonEntity.java
@@ -2,6 +2,7 @@ package dev.giovalgas.personmanager.entity.person;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -14,33 +15,23 @@ import java.time.LocalDate;
 @Table(name = "person")
 public class PersonEntity {
 
-  public static final String GENDER_KEY_MALE = "MALE";
-  public static final String GENDER_KEY_FEMALE = "FEMALE";
-  public static final String GENDER_KEY_OTHER = "OTHER";
-  public static final String GENDER_KEY_ANY = "ANY";
-
   @Id
   @Column(name = "person_id")
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @NonNull
   @Column(name = "name", length = 70)
   private String name;
 
-  @NonNull
   @Column(name = "gender", length = 10)
   private String gender;
 
-  @NonNull
   @Column(name = "phone_number", length = 16)
   private String phoneNumber;
 
-  @NonNull
   @Column(name = "email")
   private String email;
 
-  @NonNull
   @Column(name = "birth_date")
   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
   @JsonFormat(shape = JsonFormat.Shape.STRING)
