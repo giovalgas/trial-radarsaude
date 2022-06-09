d9ad432d08d1396ebe8c51f24073cc1b6cfb1cbf
giovalgas <giovalgascom@gmail.com>
2022-06-09 02:44:56 -0300

    Remove PageData class, use Filter instead

--- backend/src/main/java/dev/giovalgas/personmanager/controller/PersonController.java
+++ backend/src/main/java/dev/giovalgas/personmanager/controller/PersonController.java
@@ -2,7 +2,6 @@ package dev.giovalgas.personmanager.controller;
 
 import dev.giovalgas.personmanager.model.Filter;
 import dev.giovalgas.personmanager.entity.person.PersonEntity;
-import dev.giovalgas.personmanager.model.PageData;
 import dev.giovalgas.personmanager.service.PersonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -41,20 +40,13 @@ public class PersonController {
   }
 
   @GetMapping
-  public ResponseEntity<List<PersonEntity>> getAllPeopleByFilter(
-          @RequestBody(required = false) Filter filter,
-          @RequestParam(name = "pageData", required = false) PageData pageData
-  ) {
-
-    if(pageData == null) {
-      pageData = new PageData();
-    }
+  public ResponseEntity<List<PersonEntity>> getAllPeopleByFilter(@RequestBody(required = false) Filter filter) {
 
     if(filter == null) {
       filter = new Filter();
     }
 
-    return ResponseEntity.ok(personService.getAllPeopleByFilter(filter, pageData));
+    return ResponseEntity.ok(personService.getAllPeopleByFilter(filter));
   }
 
 }
--- backend/src/main/java/dev/giovalgas/personmanager/model/Filter.java
+++ backend/src/main/java/dev/giovalgas/personmanager/model/Filter.java
@@ -7,9 +7,14 @@ import lombok.Data;
 @Data
 public class Filter {
 
+  private int page = 1;
+  private int pageSize = 25;
+
   private String name = "";
   private String email = "";
+
   private String gender = Gender.ANY.toString();
+
   private boolean enabled = true;
 
 }
