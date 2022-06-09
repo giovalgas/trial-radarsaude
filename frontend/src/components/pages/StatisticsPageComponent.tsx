1e2bd45657fa1dd1e3b47ad4d0975eadc4dabbae
giovalgas <giovalgascom@gmail.com>
2022-06-09 16:05:40 -0300

    Hook ResponsivePie to the backend

--- frontend/src/components/pages/StatisticsPageComponent.tsx
+++ frontend/src/components/pages/StatisticsPageComponent.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Breadcrumb} from "antd";
+import {ResponsivePieComponent} from "../graphic/ResponsivePieComponent";
 
 export const StatisticsPageComponent: React.FC = () => {
+
+    let url = new URL(`http://localhost:8080/api/v1/person/stats/gender-ratio`)
+    const [ data, setData ] = useState()
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                setData(res)
+            })
+    }, []);
+
     return (
         <>
             <Breadcrumb style={{margin: '16px 0',}}>
                 <Breadcrumb.Item>Home</Breadcrumb.Item>
                 <Breadcrumb.Item>Statisticas</Breadcrumb.Item>
             </Breadcrumb>
-            <div className="site-layout-content">
-                Content
+            <div className="site-layout-content" style={{
+                height: "80vh",
+                width: "95vw",
+                margin: "auto"
+            }}>
+                {data && <ResponsivePieComponent data={data} ></ResponsivePieComponent>}
             </div>
         </>
     );
